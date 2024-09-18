004124495e853a612b283aefe8ffd4d3a81c90ae
Josh Crawford <joshua@sgroup.com.au>
2024-09-19 09:59:22 +1000

    Fix an error for multi-site installs, where “Redirect to Entry” was enabled

--- src/resources/src/js/expanded-singles.js
+++ src/resources/src/js/expanded-singles.js
@@ -10,14 +10,14 @@
     var $siteMenuBtn = $('#global-header #crumbs .menubtn[aria-controls="site-crumb-menu"');
 
     // Get the current site, as selected by the user, or stored in Cookie/LocalStorage
-    var storedSiteId = Craft.cp.getSiteId();
+    var storedSiteId = Craft.cp.getSiteId() || Craft.primarySiteId;
 
     var updateSingleUrls = function(siteId = null) {
         $('#main-content #sidebar nav a[data-cp-nav]').each(function(i, e) {
             var siteUrls = $(this).data('site-urls');
             var url = siteUrls[siteId];
 
-            console.log('Expanded Singles: ' + siteId + ': ' + url);
+            // console.log('Expanded Singles: ' + siteId + ': ' + url);
 
             if (!url) {
                 return;
@@ -36,38 +36,7 @@
     }
 
     Garnish.requestAnimationFrame($.proxy(function() {
-        // If we have a site menu
-        if ($siteMenuBtn.length) {
-            this.siteMenu = $siteMenuBtn.menubtn().data('menubtn').menu;
-
-            // If there's no Cookie/LocalStorage set for the site, fetch it from the DOM
-            if (!storedSiteId) {
-                // Get the selected menu item
-                $.each(this.siteMenu.$options, function(index, element) {
-                    var $option = $(element);
-
-                    if ($option.hasClass('sel')) {
-                        storedSiteId = $option.data('site-id');
-
-                        // While we're at it, set it to a Cookie
-                        Craft.cp.setSiteId(storedSiteId);
-                    }
-                });
-            }
-
-            // Set links to stored siteId
-            updateSingleUrls(storedSiteId);
-
-            // Add listener when selecting site
-            this.siteMenu.on('optionselect', function(e) {
-                var $option = $(e.selectedOption);
-
-                updateSingleUrls($option.data('site-id'));
-            });
-        } else {
-            // Set links to the primary site as default
-            updateSingleUrls(Craft.primarySiteId);
-        }
+        updateSingleUrls(storedSiteId);
     }, this));
 
 })(jQuery);
